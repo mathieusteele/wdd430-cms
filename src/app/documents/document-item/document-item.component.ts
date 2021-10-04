596cdcae33a5895d37c1c202d59f23d6c83661d8
Mathieu Steele <matt@steeleagency.com>
2021-10-04 08:22:14 -0400

    allow selection of a document from a list

--- src/app/documents/document-item/document-item.component.ts
+++ src/app/documents/document-item/document-item.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-item',
   templateUrl: './document-item.component.html',
-  styleUrls: ['./document-item.component.css']
+  styleUrls: ['./document-item.component.css'],
 })
 export class DocumentItemComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<void>();
+  @Input() document!: Document;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setSelectedDocument() {
+    this.selectedDocumentEvent.emit();
   }
-
 }
--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
+import { Document } from '../document.model';
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  styleUrls: ['./document-list.component.css'],
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  constructor() { }
+  documents: Document[] = [
+    {
+      id: '1',
+      name: 'Application',
+      description: 'A PDF Application',
+      url: 'https://www.example.com/',
+      children: null,
+    },
+    {
+      id: '2',
+      name: 'Questionnaire',
+      description: 'Ask these questions',
+      url: 'https://www.example.com/',
+      children: null,
+    },
+    {
+      id: '3',
+      name: 'Handbook',
+      description: 'A Guide for reference',
+      url: 'https://www.example.com/',
+    },
+    {
+      id: '4',
+      name: 'Incident Report',
+      description: 'Fill this out when something impactful happens',
+      url: 'https://www.example.com/',
+    },
+    {
+      id: '5',
+      name: 'Syllabus',
+      description: 'course learning outcomes and schedule are listed here.',
+      url: 'https://www.example.com/',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onDocumentSelected(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
-
 }
