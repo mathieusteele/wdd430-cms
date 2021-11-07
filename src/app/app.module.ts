c84f589e26b11748b5321397e790eb1770c3b810
Mathieu Steele <matt@steeleagency.com>
2021-11-06 23:28:37 -0400

    Week 8 - Forms!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { MessageEditComponent } from './messages/message-edit/message-edit.compo
 import { DropdownDirective } from './shared/dropdown.directive';
 import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DndModule } from 'ng2-dnd';
 
 @NgModule({
   declarations: [
@@ -38,7 +40,13 @@ import { ContactEditComponent } from './contacts/contact-edit/contact-edit.compo
     DocumentEditComponent,
     ContactEditComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DndModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
   templateUrl: './document-edit.component.html',
-  styleUrls: ['./document-edit.component.css']
+  styleUrls: ['./document-edit.component.css'],
 })
-export class DocumentEditComponent implements OnInit {
+export class DocumentEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') documentForm!: NgForm;
+  subscription!: Subscription;
+  originalDocument!: Document | null;
+  document!: Document;
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      let id = params.id;
+      if (!id || id == null) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalDocument = this.documentService.getDocument(id);
+
+      if (this.originalDocument == null || !this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+      console.log(this.document);
+
+      // this.documentForm?.setValue({
+      //   name: this.document.name,
+      //   description: this.document.description,
+      //   url: this.document.url,
+      // });
+    });
   }
 
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newDocument = new Document(
+      value.id,
+      value.name,
+      value.description,
+      value.url,
+      null
+    );
+    if (this.editMode && this.originalDocument) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.onClear();
+    this.router.navigate(['/documents']);
+  }
+
+  onClear() {
+    this.documentForm.reset();
+    this.editMode = false;
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
 }
